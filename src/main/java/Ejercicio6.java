98493cb69e7927f6ff1a5bdb24aa7d0fef62f2ff
Miguel Ángel Larinto <147139169+MiguelLarintoo@users.noreply.github.com>
2025-03-13 00:18:40 +0100

    He añadido el contar palabras en la actividad 6 y ademas de corregir un erro que era que ha hacer el split podria tener cualquier carater raro y te lo cogia como una palabra entera y no contaba como palabra repetida y lo he solucionado con ponerle caracteres especiales al split

--- src/main/java/Ejercicio6.java
+++ src/main/java/Ejercicio6.java
@@ -1,22 +1,40 @@
 package main.java;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
-
+		String frase = "Pues ná, que me dijo no ni ná";
+        System.out.println(palabrasDuplicadas(frase)); 
 	}
+	
+	public static Set<String> palabrasDuplicadas(String str) {
+        Map<String, Integer> frecuenciaPalabras = contarFrecuenciaPalabras(str);
+        Set<String> palabrasRepetidas = new HashSet<>();
 
+        for (String palabra : frecuenciaPalabras.keySet()) {
+            int frecuencia = frecuenciaPalabras.get(palabra); 
+            if (frecuencia > 1) {
+                palabrasRepetidas.add(palabra);
+            }
+        }
+
+        return palabrasRepetidas;
+    }
+	
+	
 	public static Map<String, Integer> contarFrecuenciaPalabras(String str) {
-		Map<String, Integer> frecuenciaPalabras = new HashMap<>();
-		if (!(str == null || str.isEmpty())) {
-			String[] palabras = str.toLowerCase().split("\\s+");
-			for (String palabra : palabras) {
-				frecuenciaPalabras.put(palabra, frecuenciaPalabras.getOrDefault(palabra, 0) + 1);
-			}
-		}
-		return frecuenciaPalabras;
-	}
+		 Map<String, Integer> frecuenciaPalabras = new HashMap<>();
+	        if (str != null && !str.isEmpty()) {
+	            String[] palabras = str.toLowerCase().split("\\s+|[,;:.!?]+");
+	            for (String palabra : palabras) {
+	                frecuenciaPalabras.put(palabra, frecuenciaPalabras.getOrDefault(palabra, 0) + 1);
+	            }
+	        }
+	        return frecuenciaPalabras;
+	    }
 }
