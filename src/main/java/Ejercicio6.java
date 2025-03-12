9ec544460374b63d390d58449f73dbac076381a9
Miguel Ángel Larinto <147139169+MiguelLarintoo@users.noreply.github.com>
2025-03-13 00:01:27 +0100

    Añadimos el metodo de crear frecuencia de palabras al ejercicio 6

--- src/main/java/Ejercicio6.java
+++ src/main/java/Ejercicio6.java
@@ -8,7 +8,15 @@ public class Ejercicio6 {
 	public static void main(String[] args) {
 
 	}
-	public static Map<String, Integer> contarFrecuenciaPalabras(String str) {
 
-}
+	public static Map<String, Integer> contarFrecuenciaPalabras(String str) {
+		Map<String, Integer> frecuenciaPalabras = new HashMap<>();
+		if (!(str == null || str.isEmpty())) {
+			String[] palabras = str.toLowerCase().split("\\s+");
+			for (String palabra : palabras) {
+				frecuenciaPalabras.put(palabra, frecuenciaPalabras.getOrDefault(palabra, 0) + 1);
+			}
+		}
+		return frecuenciaPalabras;
+	}
 }
